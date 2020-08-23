7cfcbea81678c2eba483bbe498038408433cdffb
moyui <gxy1510@gmail.com>
2020-08-23 18:07:34 +0800

    fix: config

--- config/index.js
+++ config/index.js
@@ -1,18 +1,23 @@
+const smmsConfig = require("../config").smms;
+
 const pixiv = "https://www.pixiv.net/";
 const smms = "https://sm.ms/api/v2/";
 
 module.exports = {
   rank: pixiv + "ranking.php",
-  smms: {
-    username: "moyui",
-    password: "cS28672iH3eUfAB",
-    api: {
-        getToken: smms + 'token',
-        getProfile: smms + 'profile',
-        upload: smms + 'upload',
-        createAlbum: smms + 'albums',
-        getAlbum: smms + 'albums',
-        getAlbumItems: smms + 'albums/'
+  smms: Object.assign(
+    {
+      username: "",
+      password: "",
+      api: {
+        getToken: smms + "token",
+        getProfile: smms + "profile",
+        upload: smms + "upload",
+        createAlbum: smms + "albums",
+        getAlbum: smms + "albums",
+        getAlbumItems: smms + "albums/",
+      },
     },
-  },
+    smmsConfig
+  ),
 };
